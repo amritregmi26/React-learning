7fd75c53d6dded7c12e3edba59cbd6150b30a195
amritregmi26 <amrregmi@gmail.com>
2023-10-06 17:47:37 +0545

    basic react app to render user data has been created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import userData from './userdata';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Card = (props) => {
+    return(
+        <>
+        <div className='card'>
+            <h2> Name: {props.name} </h2>
+            <h2> Age: {props.age} </h2>
+            <h3> Address: {props.addr} </h3>
+            </div>
+        </>
+    );
+}
+
+const App = () => {
+    return (
+        <>
+        <Card name = {userData[0].name} 
+              age = {userData[0].age}
+              addr = {userData[0].Address}
+        />
+        <Card name = {userData[1].name} 
+              age = {userData[1].age}
+              addr = {userData[1].Address}
+        />
+        <Card name = {userData[2].name} 
+              age = {userData[2].age}
+              addr = {userData[2].Address}
+        />
+        <Card name = {userData[3].name} 
+              age = {userData[3].age}
+              addr = {userData[3].Address}
+        />
+        <Card name = {userData[4].name} 
+              age = {userData[4].age}
+              addr = {userData[4].Address}
+        />
+        <Card name = {userData[5].name} 
+              age = {userData[5].age}
+              addr = {userData[5].Address}
+        />
+        <Card name = {userData[6].name} 
+              age = {userData[6].age}
+              addr = {userData[6].Address}
+        />
+        </>
+    );
 }
 
 export default App;
---
+++ src/userdata.js
@@ -0,0 +1,39 @@
+const userData = [
+    {
+        name: "Amrit Regmi",
+        age: 20,
+        Address: 'ThakThake'
+    },
+    {
+        name: "John Doe",
+        age: 29,
+        Address: 'Seattle'
+    },
+    {
+        name: "Jane Doe",
+        age: 22,
+        Address: 'England'
+    },
+    {
+        name: "John Wick",
+        age: 35,
+        Address: 'Sydney'
+    },
+    {
+        name: "Craig Young",
+        age: 23,
+        Address: 'Mumbai'
+    },
+    {
+        name: "Rohan Gavaskar",
+        age: 47,
+        Address: 'Delhi'
+    },
+    {
+        name: "Sachin Tendulkar",
+        age: 24,
+        Address: 'Mumbai'
+    },
+];
+
+export default userData;
